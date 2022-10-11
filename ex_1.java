677c8b3729a3ab47a96a24fd635835d2172a0bd0
nick-ju <juh_nmartins@hotmail.com>
2022-10-10 23:43:49 -0300

    Initial commit

---
+++ ex_1.java
@@ -0,0 +1,9 @@
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+package ex_1;
+
+public class ex_1 {
+    public static void main(String[]args) {
+        System.out.println("Alo mundo");
+    }
+}
